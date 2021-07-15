cd9fa77bef0bdab56fc7eda143151421488d42e3
Awni <awnyaltabaa@gmail.com>
2021-07-14 23:42:20 -0400

    completed collision exercise

--- collision/collision_checker/collision_checker.py
+++ collision/collision_checker/collision_checker.py
@@ -12,6 +12,10 @@ from dt_protocols import (
     Rectangle,
 )
 
+import shapely.geometry
+import shapely.affinity
+
+
 __all__ = ["CollisionChecker"]
 
 
@@ -33,18 +37,73 @@ class CollisionChecker:
         context.write("response", result)
 
 
+def pose_distance(pose1: FriendlyPose, pose2: FriendlyPose):
+    x1, y1, x2, y2 = pose1.x, pose1.y, pose2.x, pose2.y
+    return ((x1-x2)**2 + (y1-y2)**2)**(1/2)
+
+def get_shapely_rect(pprim: PlacedPrimitive):
+    rect = pprim.primitive
+    
+    origin = (pprim.pose.x, pprim.pose.y)
+
+    # shapely_rect = shapely.geometry.box(rect.xmin, rect.ymin, rect.xmax, rect.ymax)
+    # shapely_rect = shapely.affinity.translate(shapely_rect, xoff=pprim.pose.x, yoff=pprim.pose.y) # stackoverflow suggestion
+    # shapely_rect = shapely.affinity.rotate(shapely_rect, pprim.pose.theta_deg, origin=origin)
+
+    shapely_rect = shapely.geometry.box(rect.xmin, rect.ymin, rect.xmax, rect.ymax)
+    shapely_rect = shapely.affinity.rotate(shapely_rect, pprim.pose.theta_deg, origin=(0,0))
+    shapely_rect = shapely.affinity.translate(shapely_rect, xoff=pprim.pose.x, yoff=pprim.pose.y) # stackoverflow suggestion
+    
+    return shapely_rect
+
+def get_shapely_circ(pprim: PlacedPrimitive):
+    r = pprim.primitive.radius
+    x = pprim.pose.x
+    y = pprim.pose.y
+    
+    shapely_circ = shapely.geometry.Point(x, y).buffer(r)
+    return shapely_circ
+
+def rec_rec_collision(pprim1: PlacedPrimitive, pprim2: PlacedPrimitive) -> bool:
+
+    shapely_rect1 = get_shapely_rect(pprim1)
+    shapely_rect2 = get_shapely_rect(pprim2)
+    
+    aoi = shapely_rect1.intersection(shapely_rect2).area
+    # print(aoi)
+    
+    return aoi > 0
+
+def rec_circ_collision(pprim_rec: PlacedPrimitive, pprim_circ) -> bool:
+    shapely_rect = get_shapely_rect(pprim_rec)
+    shapely_circ = get_shapely_circ(pprim_circ)
+    
+    aoi = shapely_rect.intersection(shapely_circ).area
+    # print(aoi)
+    
+    return aoi > 0
+
+def circ_circ_collision(pprim_circ1, pprim_circ2):
+    pose1 = pprim_circ1.pose
+    pose2 = pprim_circ2.pose
+    d = pprim_circ1.primitive.radius + pprim_circ2.primitive.radius
+    
+    return pose_distance(pose1, pose2) < d
+
+def rototranslate_rb(rb, robot_pose):
+    rb.pose = robot_pose
+    return rb
+
 def check_collision(
     Wcoll: List[PlacedPrimitive], robot_body: List[PlacedPrimitive], robot_pose: FriendlyPose
 ) -> bool:
     # This is just some code to get you started, but you don't have to follow it exactly
 
-    # start by rototranslating the robot parts by the robot pose
-    rototranslated_robot: List[PlacedPrimitive] = []  #
+    rototranslated_robot: List[PlacedPrimitive] = [rototranslate_rb(rb, robot_pose) for rb in robot_body]
 
     collided = check_collision_list(rototranslated_robot, Wcoll)
 
-    # return a random choice
-    return random.uniform(0, 1) > 0.5
+    return collided
 
 
 def check_collision_list(A: List[PlacedPrimitive], B: List[PlacedPrimitive]) -> bool:
@@ -57,13 +116,17 @@ def check_collision_list(A: List[PlacedPrimitive], B: List[PlacedPrimitive]) -> 
     return False
 
 
-def check_collision_shape(a: PlacedPrimitive, b: PlacedPrimitive) -> bool:
-    # This is just some code to get you started, but you don't have to follow it exactly
-
-    if isinstance(a, Circle) and isinstance(b, Circle):
-        ...
-    if isinstance(a, Circle) and isinstance(b, Rectangle):
-        ...
-    # for now let's return a random guess
-
-    return ...
+def check_collision_shape(a: PlacedPrimitive, b: PlacedPrimitive) -> bool:    
+    
+    if isinstance(a.primitive, Circle) and isinstance(b.primitive, Circle):
+        return circ_circ_collision(a, b)
+    if isinstance(a.primitive, Circle) and isinstance(b.primitive, Rectangle):
+        return rec_circ_collision(b, a)
+    if isinstance(a.primitive, Rectangle) and isinstance(b.primitive, Circle):
+        return rec_circ_collision(a, b)
+    if isinstance(a.primitive, Rectangle) and isinstance(b.primitive, Rectangle):
+        return rec_rec_collision(a, b) 
+    
+    print('None of the checks passed')
+    
+    return None
